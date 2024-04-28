d6ba206c9e904f7f8258462f3d48e65807940030
wswearingin <williamtswearingin@gmail.com>
2024-04-27 22:27:27 -0400

    finished script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
 #creates a new ssh user using $1 parameter
-#adds a public key from the local repo or curled from remote
+#adds a public key from remote
 #removes root ssh
 
-echo "TO-DO"
+# Ensure script is run as root
+if [["${UID}" -ne 0]]; then
+	echo "Script must be run as root."
+	exit 1
+fi
+
+# Disallow root login over SSH
+sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 no/' /etc/ssh/sshd_config
+
+# Create new user and directories
+USER = $1
+useradd -m -d /home/$USER -s /bin/bash $USER
+mkdir /home/$USER/.ssh
+
+# Get public key
+curl https://github.com/wswearingin/sys-265/linux/public-keys/id_rsa.pub >> authorized_keys
+
+# Set permissions
+chmod 700 /home/$USER/.ssh
+chmod 600 /home/$USER/.ssh/authorized_keys
+chown -R $USER:$USER /home/$USER/.ssh
+
+
